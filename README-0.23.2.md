23223d5ed90b754922efa66bb8260f00b7f60cb2
Michał Kołodziej <kolodziej.michal@gmail.com>
2024-08-27 21:55:14 +0200

    Scheduler 0.23.2 in esm and cjs format, ready to use.

---
+++ README-0.23.2.md
@@ -0,0 +1,139 @@
+# @esm-polyfill/scheduler@0.23.2
+
+React DOM in ESM format.
+
+## Install
+
+Install:
+
+```sh
+npm i react@git+file:///home/mk/github/esm-polyfill/scheduler
+# specific version
+npm i react@git+file:///home/mk/github/esm-polyfill/scheduler#semver:^0.23.2
+```
+
+## Use 
+
+Reference:
+
+```ts
+import Scheduler from 'scheduler'
+
+// ...
+
+```
+
+## Internals info
+
+### Main ESM transformation procedure
+
+```sh
+npm i --save-dev scheduler-0.23.2@npm:scheduler@0.23.2
+npm i --save-dev scheduler-types-0.23@npm:@types/scheduler@0.23
+
+
+mkdir scheduler-0.23.2
+
+cd node_modules/scheduler-types-0.23
+cp --parents *.d.ts **/*.d.ts ../../scheduler-0.23.2
+cd ../../
+cp node_modules/scheduler-types-0.23/package.json scheduler-0.23.2/package-types.json
+
+
+cp node_modules/scheduler-0.23.2/package.json package-0.23.2.json
+cp node_modules/scheduler-0.23.2/package.json scheduler-0.23.2
+cp node_modules/scheduler-0.23.2/README.md scheduler-0.23.2
+cp node_modules/scheduler-0.23.2/LICENSE scheduler-0.23.2
+
+
+npx esbuild node_modules/scheduler-0.23.2/*.js \
+    --outdir=scheduler-0.23.2/esm-development \
+    --bundle \
+    --charset=utf8 \
+    --platform=node \
+    --format=esm \
+    --packages=external \
+    --analyze \
+    --define:process.env.NODE_ENV=\"development\" \
+    --alias:scheduler=@esm-polyfill/scheduler
+
+npx esbuild node_modules/scheduler-0.23.2/*.js \
+    --outdir=scheduler-0.23.2/esm-production \
+    --bundle \
+    --charset=utf8 \
+    --platform=node \
+    --format=esm \
+    --packages=external \
+    --analyze \
+    --define:process.env.NODE_ENV=\"production\" \
+    --alias:scheduler=@esm-polyfill/scheduler
+
+
+npx esbuild node_modules/scheduler-0.23.2/*.js \
+    --outdir=scheduler-0.23.2/cjs-development \
+    --bundle \
+    --charset=utf8 \
+    --platform=node \
+    --format=cjs \
+    --packages=external \
+    --analyze \
+    --define:process.env.NODE_ENV=\"development\" \
+    --alias:scheduler=@esm-polyfill/scheduler
+
+npx esbuild node_modules/scheduler-0.23.2/*.js \
+    --outdir=scheduler-0.23.2/cjs-production \
+    --bundle \
+    --charset=utf8 \
+    --platform=node \
+    --format=cjs \
+    --packages=external \
+    --analyze \
+    --define:process.env.NODE_ENV=\"production\" \
+    --alias:scheduler=@esm-polyfill/scheduler
+```
+
+
+
+
+### Changes to package.json dependencies
+
+
+#### js oryginal dependencies
+
+```json
+{
+  "dependencies": {
+    "loose-envify": "^1.1.0"
+  }
+}
+```
+
+#### dts oryginal dependencies
+
+```json
+{
+ 
+}
+```
+
+#### esm'ed dependencies
+
+```json
+{
+  "dependencies": {
+    "scheduler": "git+file:///home/mk/github/esm-polyfill/scheduler#semver^0.23.2"
+  },
+  "devDependencies": {
+    "esbuild": "^0.23.1",
+    "scheduler-0.23.2": "npm:scheduler@^0.23.2",
+    "scheduler-types-0.23": "npm:@types/scheduler@0.23"
+  }
+}
+```
+
+Why:
+
+* `loose-envify` was used for `production` or `development` 
+  contexts, but those was incorporated into package `exports`,
+  so is not neccessary.
+
---
+++ scheduler-0.23.2/cjs-production/unstable_mock.js
@@ -0,0 +1,330 @@
+"use strict";
+var __getOwnPropNames = Object.getOwnPropertyNames;
+var __commonJS = (cb, mod) => function __require() {
+  return mod || (0, cb[__getOwnPropNames(cb)[0]])((mod = { exports: {} }).exports, mod), mod.exports;
+};
+
+// node_modules/scheduler-0.23.2/cjs/scheduler-unstable_mock.production.min.js
+var require_scheduler_unstable_mock_production_min = __commonJS({
+  "node_modules/scheduler-0.23.2/cjs/scheduler-unstable_mock.production.min.js"(exports2) {
+    "use strict";
+    function f(a, b) {
+      var c = a.length;
+      a.push(b);
+      a: for (; 0 < c; ) {
+        var d = c - 1 >>> 1, e = a[d];
+        if (0 < g(e, b)) a[d] = b, a[c] = e, c = d;
+        else break a;
+      }
+    }
+    function h(a) {
+      return 0 === a.length ? null : a[0];
+    }
+    function k(a) {
+      if (0 === a.length) return null;
+      var b = a[0], c = a.pop();
+      if (c !== b) {
+        a[0] = c;
+        a: for (var d = 0, e = a.length, D = e >>> 1; d < D; ) {
+          var u = 2 * (d + 1) - 1, z = a[u], v = u + 1, E = a[v];
+          if (0 > g(z, c)) v < e && 0 > g(E, z) ? (a[d] = E, a[v] = c, d = v) : (a[d] = z, a[u] = c, d = u);
+          else if (v < e && 0 > g(E, c)) a[d] = E, a[v] = c, d = v;
+          else break a;
+        }
+      }
+      return b;
+    }
+    function g(a, b) {
+      var c = a.sortIndex - b.sortIndex;
+      return 0 !== c ? c : a.id - b.id;
+    }
+    var l = [];
+    var m = [];
+    var n = 1;
+    var p = null;
+    var q = 3;
+    var r = false;
+    var t = false;
+    var w = false;
+    var x = 0;
+    var y = null;
+    var A = null;
+    var B = -1;
+    var C = null;
+    var F = -1;
+    var G = false;
+    var H = false;
+    var I = false;
+    var J = false;
+    var K = false;
+    function L(a) {
+      for (var b = h(m); null !== b; ) {
+        if (null === b.callback) k(m);
+        else if (b.startTime <= a) k(m), b.sortIndex = b.expirationTime, f(l, b);
+        else break;
+        b = h(m);
+      }
+    }
+    function M(a) {
+      w = false;
+      L(a);
+      if (!t) if (null !== h(l)) t = true, y = N;
+      else {
+        var b = h(m);
+        null !== b && (a = b.startTime - a, A = M, B = x + a);
+      }
+    }
+    function N(a, b) {
+      t = false;
+      w && (w = false, A = null, B = -1);
+      r = true;
+      var c = q;
+      try {
+        L(b);
+        for (p = h(l); null !== p && (!(p.expirationTime > b) || a && !O()); ) {
+          var d = p.callback;
+          if ("function" === typeof d) {
+            p.callback = null;
+            q = p.priorityLevel;
+            var e = d(p.expirationTime <= b);
+            b = x;
+            "function" === typeof e ? p.callback = e : p === h(l) && k(l);
+            L(b);
+          } else k(l);
+          p = h(l);
+        }
+        if (null !== p) var D = true;
+        else {
+          var u = h(m);
+          if (null !== u) {
+            var z = u.startTime - b;
+            A = M;
+            B = x + z;
+          }
+          D = false;
+        }
+        return D;
+      } finally {
+        p = null, q = c, r = false;
+      }
+    }
+    function O() {
+      return 0 === F && null === C || -1 !== F && null !== C && C.length >= F || J && I ? G = true : false;
+    }
+    function P() {
+      if (H) throw Error("Already flushing work.");
+      if (null !== y) {
+        var a = y;
+        H = true;
+        try {
+          var b = true;
+          do
+            b = a(true, x);
+          while (b);
+          b || (y = null);
+          return true;
+        } finally {
+          H = false;
+        }
+      } else return false;
+    }
+    exports2.reset = function() {
+      if (H) throw Error("Cannot reset while already flushing work.");
+      x = 0;
+      A = y = null;
+      B = -1;
+      C = null;
+      F = -1;
+      I = H = G = false;
+    };
+    exports2.unstable_IdlePriority = 5;
+    exports2.unstable_ImmediatePriority = 1;
+    exports2.unstable_LowPriority = 4;
+    exports2.unstable_NormalPriority = 3;
+    exports2.unstable_Profiling = null;
+    exports2.unstable_UserBlockingPriority = 2;
+    exports2.unstable_advanceTime = function(a) {
+      "disabledLog" === console.log.name || K || (x += a, null !== A && B <= x && (A(x), B = -1, A = null));
+    };
+    exports2.unstable_cancelCallback = function(a) {
+      a.callback = null;
+    };
+    exports2.unstable_clearYields = function() {
+      if (null === C) return [];
+      var a = C;
+      C = null;
+      return a;
+    };
+    exports2.unstable_continueExecution = function() {
+      t || r || (t = true, y = N);
+    };
+    exports2.unstable_flushAll = function() {
+      if (null !== C) throw Error("Log is not empty. Assert on the log of yielded values before flushing additional work.");
+      P();
+      if (null !== C) throw Error("While flushing work, something yielded a value. Use an assertion helper to assert on the log of yielded values, e.g. expect(Scheduler).toFlushAndYield([...])");
+    };
+    exports2.unstable_flushAllWithoutAsserting = P;
+    exports2.unstable_flushExpired = function() {
+      if (H) throw Error("Already flushing work.");
+      if (null !== y) {
+        H = true;
+        try {
+          y(false, x) || (y = null);
+        } finally {
+          H = false;
+        }
+      }
+    };
+    exports2.unstable_flushNumberOfYields = function(a) {
+      if (H) throw Error("Already flushing work.");
+      if (null !== y) {
+        var b = y;
+        F = a;
+        H = true;
+        try {
+          a = true;
+          do
+            a = b(true, x);
+          while (a && !G);
+          a || (y = null);
+        } finally {
+          F = -1, H = G = false;
+        }
+      }
+    };
+    exports2.unstable_flushUntilNextPaint = function() {
+      if (H) throw Error("Already flushing work.");
+      if (null !== y) {
+        var a = y;
+        J = true;
+        I = false;
+        H = true;
+        try {
+          var b = true;
+          do
+            b = a(true, x);
+          while (b && !G);
+          b || (y = null);
+        } finally {
+          H = G = J = false;
+        }
+      }
+    };
+    exports2.unstable_forceFrameRate = function() {
+    };
+    exports2.unstable_getCurrentPriorityLevel = function() {
+      return q;
+    };
+    exports2.unstable_getFirstCallbackNode = function() {
+      return h(l);
+    };
+    exports2.unstable_next = function(a) {
+      switch (q) {
+        case 1:
+        case 2:
+        case 3:
+          var b = 3;
+          break;
+        default:
+          b = q;
+      }
+      var c = q;
+      q = b;
+      try {
+        return a();
+      } finally {
+        q = c;
+      }
+    };
+    exports2.unstable_now = function() {
+      return x;
+    };
+    exports2.unstable_pauseExecution = function() {
+    };
+    exports2.unstable_requestPaint = function() {
+      I = true;
+    };
+    exports2.unstable_runWithPriority = function(a, b) {
+      switch (a) {
+        case 1:
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+          break;
+        default:
+          a = 3;
+      }
+      var c = q;
+      q = a;
+      try {
+        return b();
+      } finally {
+        q = c;
+      }
+    };
+    exports2.unstable_scheduleCallback = function(a, b, c) {
+      var d = x;
+      "object" === typeof c && null !== c ? (c = c.delay, c = "number" === typeof c && 0 < c ? d + c : d) : c = d;
+      switch (a) {
+        case 1:
+          var e = -1;
+          break;
+        case 2:
+          e = 250;
+          break;
+        case 5:
+          e = 1073741823;
+          break;
+        case 4:
+          e = 1e4;
+          break;
+        default:
+          e = 5e3;
+      }
+      e = c + e;
+      a = { id: n++, callback: b, priorityLevel: a, startTime: c, expirationTime: e, sortIndex: -1 };
+      c > d ? (a.sortIndex = c, f(m, a), null === h(l) && a === h(m) && (w ? (A = null, B = -1) : w = true, A = M, B = x + (c - d))) : (a.sortIndex = e, f(l, a), t || r || (t = true, y = N));
+      return a;
+    };
+    exports2.unstable_setDisableYieldValue = function(a) {
+      K = a;
+    };
+    exports2.unstable_shouldYield = O;
+    exports2.unstable_wrapCallback = function(a) {
+      var b = q;
+      return function() {
+        var c = q;
+        q = b;
+        try {
+          return a.apply(this, arguments);
+        } finally {
+          q = c;
+        }
+      };
+    };
+    exports2.unstable_yieldValue = function(a) {
+      "disabledLog" === console.log.name || K || (null === C ? C = [a] : C.push(a));
+    };
+  }
+});
+
+// node_modules/scheduler-0.23.2/unstable_mock.js
+if (true) {
+  module.exports = require_scheduler_unstable_mock_production_min();
+} else {
+  module.exports = null;
+}
+/*! Bundled license information:
+
+scheduler-0.23.2/cjs/scheduler-unstable_mock.production.min.js:
+  (**
+   * @license React
+   * scheduler-unstable_mock.production.min.js
+   *
+   * Copyright (c) Facebook, Inc. and its affiliates.
+   *
+   * This source code is licensed under the MIT license found in the
+   * LICENSE file in the root directory of this source tree.
+   *)
+*/
